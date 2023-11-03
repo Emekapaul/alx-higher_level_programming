4744f2ef60669a27c3d52dee931244ef84898e51
Paul Nnaemeka <emekapaul302@gmail.com>
2023-11-03 03:40:44 +0100

    input function directly into print task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,8 +5,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    result = add(a, b)
-    print(f'{a} + {b} = {result}')
+    print(f'{a} + {b} = {add(a, b)}')
 
 
 if __name__ == "__main__":

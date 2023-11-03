b9c728bd6e9e1171823684c0ddfc59fdbe3e15fe
Paul Nnaemeka <emekapaul302@gmail.com>
2023-11-03 03:36:47 +0100

    Correct task 1 & 2 0x02-python-import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
 
-addResult = add(a, b)
-subResult = sub(a, b)
-mulResult = mul(a, b)
-divResult = div(a, b)
 
-print(f'{a} + {b} = {addResult}')
-print(f'{a} - {b} = {subResult}')
-print(f'{a} * {b} = {mulResult}')
-print(f'{a} / {b} = {divResult}')
+def main():
+    a = 10
+    b = 5
+
+    addResult = add(a, b)
+    subResult = sub(a, b)
+    mulResult = mul(a, b)
+    divResult = div(a, b)
+
+    print(f'{a} + {b} = {addResult}')
+    print(f'{a} - {b} = {subResult}')
+    print(f'{a} * {b} = {mulResult}')
+    print(f'{a} / {b} = {divResult}')
+
+
+if __name__ == "__main__":
+    main()

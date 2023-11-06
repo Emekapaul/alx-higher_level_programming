b6cf898a0f18dcd5166498e54692cfe2adc3d0e7
Paul Nnaemeka <emekapaul302@gmail.com>
2023-11-06 04:43:22 +0100

    Solve task 6 of 0x03-python-data_structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+
+    for row in matrix:
+        for i, element in enumerate(row):
+            if (i != len(row) - 1):
+                print('{:d}'.format(element), end=' ')
+
+            else:
+                print('{:d}'.format(element))
